bafe58f8f9624b45add72867fe859494fbf88333
nadazkia <93324434+nadazkia@users.noreply.github.com>
2021-11-24 11:28:26 +0700

    Update dif-selisih-tengah.m

--- dif-selisih-tengah.m
+++ dif-selisih-tengah.m
@@ -1,32 +1,49 @@
 clear
 clc
-disp('Nama : Nadazkia')
-% disp('NIM  : ')
+disp('Nama : Nada Fatiyyah Azkia')
+disp('NIM  : 11180170000084')
 disp('======================================================')
 disp('          Metode Diferensiasi Selisih Tengah          ')
 disp('======================================================')
 
+%DIKETAHUI
 disp('Diketahui:')
 a=input('Batas bawah a = ');
 b=input('Batas atas b = ');
 h=input('h = ');
 n=(b-a)/h;
 sigma=0;
-f = @(x) 2*x.*exp(x); % Fungsi
-f_eksak = @(x) (2*x.*exp(x))+(2*exp(x)); % Turunan Pertama
-f_tengah = @(x,h) (f(x+h)-f(x-h))/(2*h); % Metode Selisih Tengah
+f = @(x) 5*x.^3; %Fungsi
+df = @(x) 15*x.^2; %Turunan Pertama
+f_hampiran = @(x,h) (f(x+h)-f(x-h))/(2*h); %Metode Selisih Tengah
 
+%TABELNYA
 disp('======================================================')
 disp('i       x        f tengah      f eksak      error     ')
 disp('======================================================')
 for i=0:n
     x=a+i*h;
-    ft=f_tengah(x,h);
-    fek=f_eksak(x);
+    ft=f_hampiran(x,h);
+    fek=df(x);
     e=abs(fek-ft);
     sigma=sigma+e;
-    fprintf('% d    % f    % f    % f    % f    \n', i,x,ft,fek,e)
+    fprintf('%d    %f    %f    %f    %f    \n', i,x,ft,fek,e)
 end
+
+%GRAFIK
+x=a:h:b;
+hold on
+grid on;
+
+dff=df(x); %Turunan Eksak
+plot(x,dff,'b-');
+ftt=f_hampiran(x,h);
+plot(x,ftt,'-r');
+
+legend('f Hampiran','f Eksak');
+hold off
+
+%SELESAIIIII
 rata_error= sigma/(i+1);
 disp('======================================================')
-fprintf('Rata-rata error = % f \n', rata_error)
+fprintf('Rata-rata error = %f \n', rata_error)
